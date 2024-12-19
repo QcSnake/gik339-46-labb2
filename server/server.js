382d704cef792751b9ca75c057051b1b6d60b8d9
QcSnake <Abdimalik655@gmail.com>
2024-12-19 19:55:24 +0100

    Uppgift 3
    server hämtar data från databasen och skickar den som svar via /users

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+
+const server = express();
+
+server
+  .use(express.json())
+  .use(express.urlencoded({ extended: false }))
+  .use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    next();
+  });
+
+const db = new sqlite3.Database("./gik339-labb2.db");
+const sql = "SELECT * FROM USERS";
+
+server.get("/users", (req, res) => {
+  db.all(sql, (err, rows) => {
+    if (err) {
+      res.status(500).send("Något gick fel med databasen!");
+      return;
+    }
+    res.send(rows);
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Servern körs på http://localhost:3000`);
+});
